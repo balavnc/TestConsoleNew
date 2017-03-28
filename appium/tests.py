ab139914acca4b1e4946b8c24201662c69bf2b84
Balasubramanian N <balasubramanian_n@hcl.com>
2017-03-28 15:08:54 +0530

    updated requirements.txt file

--- appium/tests.py
+++ appium/tests.py
@@ -2,13 +2,3 @@ from django.test import TestCase
 
 # Create your tests here.
 
-
-import win32com.client
-
-
-wmi = win32com.client.GetObject(r"winmgmts:")
-dev=[]
-for usb in wmi.InstancesOf(r"Win32_USBHub"):
-    dev.append(usb.DeviceID)
-
-print dev
