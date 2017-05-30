2f25c002602330fd8d078326972ebe4e6d457c4d
Balasubramanian N <balasubramanian_n@hcl.com>
2017-05-30 17:31:16 +0530

    Autorefresh appium page
    Handled RotatingFileHandler in Logger

--- appium/views.py
+++ appium/views.py
@@ -17,28 +17,28 @@ from django.views.generic.edit import CreateView, UpdateView
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Logger
 import logging
+from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'runtime/logs/appium.log')
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 
-# create a file handler
-file_handler = logging.FileHandler(filename)
+# creating file handlers
+file_handler = RotatingFileHandler(filename, mode='a', maxBytes=1*1024*1024, 
+                                 backupCount=3, encoding=None, delay=0)
 file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
+# creating console handler
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
-
-# create a logging format
-formatter = logging.Formatter('%(asctime)s [ %(levelname)s ] %(message)s')
-file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
-
-# adding handlers to logger
 logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+
 logger.addHandler(logging.NullHandler())
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
